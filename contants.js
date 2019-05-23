548470078dc6879481e1621a51d123e6d3f639e5
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-05-24 00:45:48 +0300

    Update contants.js

--- contants.js
+++ contants.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGODB_SERVER: "mongodb://localhost:27017/movie"
+    MONGODB_SERVER: "mongodb://localhost:27017/movie" // Local üzerinde çalışılacaksa bu şekilde kalmasına bir sakın yok. Ancak herhangi bir online sunucu ile çalışacaksa oradaki bağlantı metninin alınması gerekmektedir.
 }
